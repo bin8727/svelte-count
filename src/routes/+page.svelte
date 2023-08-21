bf4049df584dba007279fc8dd7450ba01483ba75
MoYuBin <bin8727@naver.com>
2023-08-21 19:59:26 +0900

    주석 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,22 @@
 	import Card from "../Card.svelte";
 	import _ from 'lodash';
 	
+	// 카드 추가 시에 갯수를 세주는 이벤트를 주기 위해 작성
 	let add: boolean = false;
+
+	// cards라는 빈 배열을 생성
 	let cards: CardType[] = [];
+
+	// 카드 벨류의 최소와 최대값 설정
 	const minValue: number = 0;
 	const maxValue: number = 99;
 
+	// 카드 추가 시에 갯수를 세주는 이벤트를 주기 위해 작성
 	const handleAdd = () => {
 		add = true;
 	};
 
+	// 추가 버튼 클릭 시 기존의 cards 객체에 concat으로 감싼 배열을 cards에 결합
 	const addCards = () => {
 		cards = cards.concat({
 			id: cards.length + 1, 
@@ -18,10 +25,16 @@
 		});
 	};
 
+	// 넘겨 받은 id와 card.id가 같으면 대상을 필터하고 새로운 배열을 만들어 리턴
 	const removeCards = (id: number) => {
 		cards = cards.filter((card) => card.id !== id);
 	};
 
+	// card.id가 넘겨받은 id와 같고 card.value가 최대값 미만인지 확인
+	// 참이라면 ...card를 이용해 card를 복사한 후 value의 값을 1 상승
+	// 거짓이라면 기존의 card를 이용
+	// sortBy를 이용해 cards배열의 value 값에 대해 오름차순으로 정렬
+	// value를 호출하여 최종 결과를 반환하고 cards에 할당
 	const incrementCardValue = (id: number) => {
   	cards = _(cards)
   	  .map((card) =>
@@ -33,6 +46,11 @@
   	  .value();
 	};
 
+	// card.id가 넘겨받은 id와 같고 card.value가 최소값 이상인지 확인
+	// 참이라면 ...card를 이용해 card를 복사한 후 value의 값을 1 하락
+	// 거짓이라면 기존의 card를 이용
+	// sortBy를 이용해 cards배열의 value 값에 대해 오름차순으로 정렬
+	// value를 호출하여 최종 결과를 반환하고 cards에 할당
 	const decrementCardValue = (id: number) => {
   	cards = _(cards)
 			.map((card) =>
@@ -44,6 +62,7 @@
 			.value();
 	};
 
+	// reactive statement cards의 길이가 변경될 때 마다 count를 업데이트
 	$: count = cards.length;
 </script>
 
